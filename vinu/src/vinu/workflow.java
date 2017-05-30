40221e39fb70aed2e07425322955b2c2a10b48dd
vinay <vk970020@gmail.com>
2017-05-29 19:33:13 -0700

    commited work flow project

---
+++ vinu/src/vinu/workflow.java
@@ -0,0 +1,25 @@
+package vinu;
+
+public class workflow {
+
+	public static void main(String[] args){
+		printThree();
+		printThree();
+		printFour();
+	
+	}
+
+	private static void printThree() {
+		// TODO Auto-generated method stub
+	    System.out.println("my life is fucked up");	
+	}
+
+	private static void printFour() {
+		// TODO Auto-generated method stub
+		printThree();
+		printThree();
+		
+	}
+
+}
+
